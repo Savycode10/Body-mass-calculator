2c5be49814579b36f370fbad5477107e30652250
Savycode10 <89041940+Savycode10@users.noreply.github.com>
2021-09-10 02:53:03 +0100

    BODY MASS INDEX calculator jupyter note book and Script

---
+++ SAVY-Copy1.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print(This program is to calculate the BMI of individuals)
+
+
+# In[ ]:
+
+
+NAME= input("Enter your name: ")
+
+
+# In[ ]:
+
+
+HEIGHT= float(input("Enter your Height in metres :"))
+
+
+# In[ ]:
+
+
+WEIGHT=  float(input("Enter your Weight in Killogram :"))
+
+
+# In[ ]:
+
+
+if BMI < 18.5: 
+    print(f"{NAME}  is under weight by BMI {BMI}")
+else:
+   print(f"{NAME} is not under weight by BMI {BMI}")
+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
